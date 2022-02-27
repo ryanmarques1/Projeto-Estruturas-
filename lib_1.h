b409a5ac1c4ad9364de4cc1d3d53926cd773b914
BTMiranda <68792743+BTMiranda@users.noreply.github.com>
2022-02-27 15:49:47 -0300

    V1

--- lib_1.h
+++ lib_1.h
@@ -1,5 +1,56 @@
 #ifndef LIB_1_H_INCLUDED
 #define LIB_1_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+/// Menu Programa
+
+/// Menu Fase
+
+void Tabuleiro(){
+    int i,j;
+    char TabMat [8][8];
+    TabMat[0][0] = 'P';
+    TabMat[7][7] = 'O';
+
+    for(i = 0; i < 8; i++){
+        printf("|---|---|---|---|---|---|---|---|\n");
+        for(j = 0; j < 8; j++){
+            if(j == 0){
+                printf("|");
+            }
+            printf(" %c |",TabMat[i][j]);
+            if(j == 7){
+                printf("\n");
+            }
+        }
+        if(i == 7){
+            printf("|---|---|---|---|---|---|---|---|\n");
+        }
+    }
+
+//    printf("| P |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   |   |\n");
+//    printf("|   |   |   |   |   |   |   | O |\n");
+//    printf("|---|---|---|---|---|---|---|---|\n");
+}
+
+/// Primeira Fase
+
+/// Segunda Fase
+
+/// Terceira Fase
+
+///Etc
+/*
+
+*/
+
 
 
 
--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib_1.h"
+
 int main()
 {
-    printf("Game for Die\n");
+    int n;
+    do{
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("=-=-=-=-=-=    Jogo    -=-=-=-=-=-=\n");
+        printf("=-=-=    Comandos Em Loop    -=-=-=\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+        printf("Menu de Opcoes\n");
+        printf("1 - Iniciar Jogo\n");
+        printf("2 - Informações\n");
+        printf("3 - \n");
+        printf("4 - Sair\n\n");
+
+        printf("Digite sua Opcao: ");
+        scanf("%d", &n);
+        printf("\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+
+        switch(n){
+        case 1:
+            Tabuleiro();
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        default:
+            printf("Opcao invalida!\n\n");
+            break;
+        }
+
+    }while(n != 4);
     return 0;
 }
+//
