d1e2d7d30658dc1657526d1bcf20bed57cdd7463
ryanmarques <47296650+ryanmarques1@users.noreply.github.com>
2022-03-10 15:54:04 -0300

    V 1.7

--- Chapter1.h
+++ Chapter1.h
@@ -6,29 +6,50 @@
 
 Estrutura Fila Dinâmica.
 */
-typedef struct Dados{
-};
-typedef struct Fila{
-};
+typedef struct Dices{
+    int NumdoComando;
+    int vezesExec;
+}infos;
 typedef struct NOS{
-};
+    struct Dices infos;
+    struct NOS* next;
+}NOF;
+typedef struct Fila{
+    NOF* ini;
+    NOF* fim;
+}sFila;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sFila* alloc_fila(){
+    return malloc(sizeof(sFila));
+}
+NOF* alloc_nos(){
+    return malloc(sizeof(NOF));
+}
+void ini_fila(sFila *f){
+    f->ini = NULL;
+    f->fim = NULL;
+}
+int fila_vazia(sFila *f){
+    if(f->ini == NULL)
+        return 1;
+    return 0;
+}
+void insere_listaComandos(sFila *f, struct Dices infos){
+    NOF* nos = NULL;
+    nos = alloc_nos();
+    if(nos != NULL){
+        nos->infos = infos;
+        nos->next = NULL;
+        if(fila_vazia(f))
+            f->ini = nos;
+        else
+            f->fim->next = nos;
+        f->fim = nos;
+    }else{
+        printf("\nErro de alocação\n");
+        exit(1);
+    }
+}
 void traps_fase1(char **TabMat){
     ///Fase 1 TRAPS.///(Completo)
     TabMat[0][7] = 'X';
--- main.c
+++ main.c
@@ -10,8 +10,16 @@
 int main()
 {
     setlocale(LC_ALL,"");
+    sFila *f =  NULL;
+    infos L;
     int op, dimensao = 8, healthPoints = 3;
     char **TabMat;
+    f = alloc_fila();
+    if(f == NULL){ //se for null, erro ao alocar, senão inicia fila.
+        printf("\nErro de alocação\n");
+        exit(1);
+    }else
+        ini_fila(f);
     do{
         menu();
         printf("Digite sua Opção: ");
@@ -21,8 +29,11 @@ int main()
         switch(op){
         case 1:
             TabMat = Create_Tabuleiro(dimensao);
-            traps_fase2(TabMat);
+            traps_fase1(TabMat);
             Impress_Tabuleiro(TabMat,dimensao);
+            /*printf("Digite o numero referente a lista de comandos e as vezes que será executado: ");
+            scanf("%d %d", &L.NumdoComando, &L.vezesExec);
+            insere_listaComandos(f,L);*/
             break;
         case 2:
             Instrucoes();
